0f47b4f346b314567b47e0872130aa00197ba4ca
iatenothingbutriceforthreedays <darwinvickers@gmail.COM>
2021-12-02 14:53:32 -0500

    nicer interactivity

--- ratloving/Assets/Scripts/MouseAwareRat.cs
+++ ratloving/Assets/Scripts/MouseAwareRat.cs
@@ -7,7 +7,9 @@ public class MouseAwareRat : MonoBehaviour
     public Camera camera;
     public Animator animator;
 
-    public float maxAngle;
+    //public float maxAngle;
+    public float rotationStrength;
+
     public float radius;
 
     private Quaternion initRot;
@@ -26,8 +28,19 @@ public class MouseAwareRat : MonoBehaviour
             Input.mousePosition.y/Screen.height,
             0f
         );
+
+        Vector3 mouseDelta = new Vector3 (
+            Input.GetAxis("Mouse X"),
+            Input.GetAxis("Mouse Y"),
+            0f
+        );
+#if UNITY_WEBGL && !UNITY_EDITOR
+        mouseDelta *= 0.05f;
+#endif
+
         Vector3 rat = camera.WorldToViewportPoint(transform.position);
         rat.z = 0f;
+
 
         float distance = Vector3.Distance(mouse, rat);
 
@@ -37,14 +50,21 @@ public class MouseAwareRat : MonoBehaviour
         //     animator.Play("anim2");
         // }
 
+
+        // Vector3.Cross(transform.up, mouseDelta)
+
         float x = distance/1.41f; // [-0.5, 0.5]
         float r = radius;
 
         float t = 1f - (x*x)/(r*r);
         t = Mathf.Clamp(t, 0f, 1f);
+
+        // Rotate rat in direction of mouse movement
+        Quaternion rot = Quaternion.FromToRotation(Vector3.forward, Vector3.forward + mouseDelta*t*rotationStrength);
+        transform.localRotation *= rot;
         
 
-        transform.localRotation = initRot*Quaternion.AngleAxis(t*maxAngle, transform.forward);
+        // transform.localRotation = initRot*Quaternion.AngleAxis(t*maxAngle, transform.forward);
         
         //animator.SetFloat("Speed", t);
 
